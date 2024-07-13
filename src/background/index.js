b0d225aa58054c873ad295bf1fb23352bc965005
Vitalii Elenhaupt <velenhaupt@gmail.com>
2024-07-13 21:14:50 +0200

    Basic logic to show buttons

--- src/background/index.js
+++ src/background/index.js
@@ -1,7 +1,12 @@
 console.log('background is running')
 
-chrome.runtime.onMessage.addListener((request) => {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'COUNT') {
     console.log('background has received a message from popup, and count is ', request?.count)
+  }
+  if (request.type === 'generate') {
+    console.log(request)
+    // send response to content script
+    sendResponse({ type: 'generate', data: request.data })
   }
 })
--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -1,14 +1,66 @@
 const selectors = {
   addStepButton: '[type=button][aria-label="Add a Step"]',
+  deleteStepButton: '[type=button][aria-label="Delete Step"]',
+  generateButton: '[type=button][aria-label="Generate with AI"]',
+
+  popupDeleteButton: '[type=button][aria-label="Delete"]',
 }
 
 const events = {
   addStepButtonReady: 'GarminAddStepButtonReady',
+  newPromptFired: 'GarminNewPromptAdded',
+  deleteStepPopupFired: 'GarminDeleteStepPopupFired',
 }
 
 document.addEventListener(events.addStepButtonReady, () => {
-  console.log(document.querySelector(selectors.addStepButton))
+  addGenerateButton()
 })
+
+document.addEventListener(events.newPromptFired, (event) => {
+  const generateButton = document.querySelector(selectors.generateButton)
+  generateButton.disabled = true
+
+  chrome.runtime.sendMessage({ type: 'generate', prompt: event.detail }, (response) => {
+    deleteSteps()
+    generateButton.disabled = false
+  })
+})
+
+document.addEventListener(events.deleteStepPopupFired, ({ detail: { target } }) => {
+  target.click()
+  debugger
+  deleteSteps()
+})
+
+function deleteSteps() {
+  const deleteStepButtons = document.querySelectorAll(selectors.deleteStepButton)
+  deleteStepButtons.forEach((button) => {
+    button.click()
+  })
+}
+
+function addGenerateButton(text = 'Generate with AI') {
+  const addStepButton = document.querySelector(selectors.addStepButton)
+  if (!addStepButton) {
+    return
+  }
+
+  if (document.querySelector(selectors.generateButton)) {
+    return
+  }
+
+  const newButton = addStepButton.cloneNode(true)
+  newButton.addEventListener('click', () => {
+    const result = prompt('Describe your workout:', '10m warmup, 5x(5m 300wt, 5m 120w)')
+    if (result) {
+      document.dispatchEvent(new Event(events.newPromptFired, { detail: result }))
+    }
+  })
+  newButton.style.backgroundColor = 'green'
+  newButton.textContent = text
+  newButton.ariaLabel = text
+  addStepButton.parentElement.appendChild(newButton)
+}
 
 function waitPageLoaded() {
   let MutationObserver = window.MutationObserver || window.WebKitMutationObserver
@@ -18,11 +70,23 @@ function waitPageLoaded() {
       return
     }
 
-    const mutation = mutations.find((mutation) => mutation.target.matches(selectors.addStepButton))
+    // const mutation = mutations.find((mutation) => mutation.target.matches(selectors.addStepButton))
 
-    if (mutation) {
-      document.dispatchEvent(new Event(events.addStepButtonReady))
-    }
+    // if (mutation) {
+    //   document.dispatchEvent(new Event(events.addStepButtonReady))
+    // }
+    mutations.forEach((mutation) => {
+      if (mutation.target.matches(selectors.addStepButton)) {
+        document.dispatchEvent(new Event(events.addStepButtonReady))
+      }
+
+      console.log(mutation)
+      if (mutation.target.matches(selectors.popupDeleteButton)) {
+        document.dispatchEvent(
+          new CustomEvent(events.deleteStepPopupFired, { detail: { target: mutation.target } }),
+        )
+      }
+    })
   })
 
   observer.observe(document.getElementsByTagName('body')[0], {
